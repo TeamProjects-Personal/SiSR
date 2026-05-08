e1829c1f63c5ce6cb5d6157e541ab1588e521bb3
Sarthak <Sarthakmp007@gmail.com>
2026-05-09 05:08:23 +0530

    Added SRCNN
    Not Tested yet

---
+++ CNN/ConvolutionNeuralNetwork.py
@@ -0,0 +1,138 @@
+import numpy as np
+import SRCNN 
+
+class ConvolutionNeuralNetwork:
+
+    def he_init(self,kh, kw, cin, cout):
+        scale = np.sqrt(2.0 / cin)
+        return np.random.randn(kh, kw, cin, cout) * scale
+
+    def __init__(self, num_filter, learning_rate=0.1):
+
+        self.learning_rate = learning_rate
+
+        # Bias vectors
+        self.biases = [
+            np.zeros((num_filter, 1))
+        ]
+
+        num_filters = 10
+        kernel_size = 3
+        in_channels = 3
+
+        self.kernels = np.random.randn(
+            num_filters,
+            kernel_size,
+            kernel_size,
+            in_channels
+        )
+
+        
+
+    def activation(self, x):
+        return np.maximum(0,x)
+
+    def activation_derivative(self, x):
+
+        sig = self.activation(x)
+
+        return sig * (1 - sig)
+
+
+    def predict(self, X,K):
+
+        for w, b in zip(self.weights, self.biases):
+
+            z = SRCNN.Convolution(X,K) + b
+
+            a = self.activation(z)
+
+        return a
+
+    def compute_grad_kernel(x, delta, kH, kW, C_in, C_out):
+
+        H, W, _ = x.shape
+        H_out, W_out, _ = delta.shape
+
+        grad_k = np.zeros((C_out, kH, kW, C_in))
+
+        for f in range(C_out):
+            for i in range(H_out):
+                for j in range(W_out):
+
+                    for cin in range(C_in):
+                        grad_k[f, :, :, cin] += (
+                            x[i:i+kH, j:j+kW, cin] * delta[i, j, f]
+                        )
+
+        return grad_k
+
+    def cost(self, output, y):
+        return np.mean((output - y) ** 2)
+
+    def backprop(self, x,k, y):
+
+        # Forward pass
+        activations,zs = SRCNN.ConvoLayer(x,k,self.biases)
+
+        A_L = activations
+        Z_L = zs
+
+        H,W,n_filter = activations.shape
+        kn,kh,kw,kc = k.shape
+        delta = (A_L - y)*self.activation_derivative(Z_L)
+
+        grad_b = np.zeros(n_filter)
+        grad_kernel = self.compute_grad_kernel(x,delta,kh,kw,n_filter,kc)
+        for l in reversed(range(n_filter)):
+
+            
+            grad_b[l] = np.sum(delta[:,:,l])
+
+            if(l>0):
+                delta =  SRCNN.conv_transpose(delta,k)
+                delta *= self.activation_derivative(zs[:,:,l-1])
+
+                
+
+
+
+    def train(self, X_train, Y_train, epochs):
+
+        for epoch in range(epochs):
+
+            total_loss = 0
+
+            for x, y in zip(X_train, Y_train):
+
+
+                activations, zs = SRCNN.ConvoLayer(x, self.kernels, self.biases)
+
+
+                loss = np.mean((activations - y) ** 2)
+                total_loss += loss
+
+
+                grad_kernel, grad_b = self.backprop(x, self.kernels, y)
+
+
+                self.kernels -= self.learning_rate * grad_kernel
+                self.biases  -= self.learning_rate * grad_b
+
+            print(f"Epoch {epoch+1}, Loss: {total_loss / len(X_train)}")
+    
+    def save_model(self, filename):
+
+        np.savez(
+            filename,
+            weights=np.array(self.weights, dtype=object),
+            biases=np.array(self.biases, dtype=object)
+        )
+    def load_model(self, filename):
+
+        data = np.load(filename, allow_pickle=True)
+
+        self.weights = list(data["weights"])
+
+        self.biases = list(data["biases"])
+
--- CNN/NeuralNetwork.py
+++ CNN/NeuralNetwork.py
@@ -1,37 +1,157 @@
 import numpy as np
-import random
-import math
 
-class NeuralNetwork1:
-    def __init__(self,layer_sizes):
-    
-        weight_shapes = [(a,b) for a,b in zip(layer_sizes[1:],layer_sizes[:-1])]
-        self.weights = [np.round(np.random.standard_normal(s)/s[1]**0.5,2) for s in weight_shapes]
-        self.biases = [np.zeros((s,1)) for s in layer_sizes[1:]] 
+class NeuralNetwork:
 
-    def predict(self,a):
-        for w,b in zip(self.weights,self.biases):
-            a = self.activation((w @ a + b))
+    def __init__(self, layer_sizes, learning_rate=0.1):
+
+        self.learning_rate = learning_rate
+
+        # Weight matrices
+        weight_shapes = [
+            (a, b) for a, b in zip(layer_sizes[1:], layer_sizes[:-1])
+        ]
+
+        
+        self.weights = [
+            np.random.randn(*s) * np.sqrt(1 / s[1])
+            for s in weight_shapes
+        ]
+
+        # Bias vectors
+        self.biases = [
+            np.zeros((s, 1))
+            for s in layer_sizes[1:]
+        ]
+        
+        #self.load_model("Digits_Recongnization.npz");
+
+
+
+
+    def activation(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def activation_derivative(self, x):
+
+        sig = self.activation(x)
+
+        return sig * (1 - sig)
+
+
+    def predict(self, a):
+
+        for w, b in zip(self.weights, self.biases):
+
+            z = w @ a + b
+
+            a = self.activation(z)
+
         return a
 
-    def activation(self,x):
-        return 1 / (1 + np.exp(-x)) 
 
-    def print_accuracy(self, images,lables):
-        prediction = self.predict(images)
-        correct = sum([np.argmax(a) ==  np.argmax(b) for a,b in zip(prediction,lables)])
-        print("{0}/{1} : acc = {2}".format(correct,len(images),(correct/len(images)*100)))
+    def forward(self, x):
 
-    def cost(self,a,y):
-        diff = (a - y)**2 # for all a & y (j) elements
-        r_cost = sum(diff);
-        return r_cost
-        
-    def backprop(self,x,b,w1):   
-        z = w1 @ x
-        
-                
-        
-        
+        activations = [x]
+        zs = []
 
+        a = x
+
+        for w, b in zip(self.weights, self.biases):
+
+            z = w @ a + b
+
+            zs.append(z)
+
+            a = self.activation(z)
+
+            activations.append(a)
+
+        return activations, zs
+
+
+    def cost(self, output, y):
+        return np.mean((output - y) ** 2)
+
+    def backprop(self, x, y):
+
+        # Forward pass
+        activations, zs = self.forward(x)
+
+
+        grad_w = [np.zeros_like(w) for w in self.weights]
+        grad_b = [np.zeros_like(b) for b in self.biases]
+
+
+        dA = (
+            (activations[-1] - y)
+            * self.activation_derivative(zs[-1])
+        )
+
+        # Gradients for last layer
+        grad_w[-1] = delta @ activations[-2].T
+        grad_b[-1] = delta
+
+
+        for l in range(2, len(self.weights) + 1):
+
+            z = zs[-l]
+
+            sp = self.activation_derivative(z)
+
+            delta = (self.weights[-l + 1].T @ delta) * sp
+
+            grad_w[-l] = delta @ activations[-l - 1].T
+
+            grad_b[-l] = delta
+
+
+        for i in range(len(self.weights)):
+
+            self.weights[i] -= self.learning_rate * grad_w[i]
+
+            self.biases[i] -= self.learning_rate * grad_b[i]
+
+    def train(self, X, Y, epochs):
+
+        for epoch in range(epochs):
+
+            for x, y in zip(X, Y):
+
+                # Convert to column vectors
+                x = np.array(x).reshape(-1, 1)
+                y = np.array(y).reshape(-1, 1)
+
+                self.backprop(x, y)
+
+            # Print loss every 100 epochs
+            if epoch % 100 == 0:
+
+                total_loss = 0
+
+                for x, y in zip(X, Y):
+
+                    x = np.array(x).reshape(-1, 1)
+                    y = np.array(y).reshape(-1, 1)
+
+                    pred = self.predict(x)
+
+                    total_loss += self.cost(pred, y)
+
+                print(f"Epoch {epoch} Loss = {total_loss:.4f}")
+    
+    def save_model(self, filename):
+
+        np.savez(
+            filename,
+            weights=np.array(self.weights, dtype=object),
+            biases=np.array(self.biases, dtype=object)
+        )
+    
+    def load_model(self, filename):
+
+        data = np.load(filename, allow_pickle=True)
+
+        self.weights = list(data["weights"])
+
+        self.biases = list(data["biases"])
 
